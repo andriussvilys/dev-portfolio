74d6a761153991d1d8b3fdc1a8a8851510424ca9
andriussvilys <andriussvilys@gmail.com>
2024-07-24 13:37:52 +0100

    tags create

--- src/app/dashboard/tags/create/page.tsx
+++ src/app/dashboard/tags/create/page.tsx
@@ -1,6 +1,4 @@
 "use client"
-import { usePathname } from "next/navigation"
-import { FormEvent, useState } from "react"
 import {upload} from "@/src/lib/tags"
 import TagForm from "@/src/components/tags/tagForm"
 
