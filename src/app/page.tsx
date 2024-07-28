094a36fb3baa1999413a5c5d5272812ae0fbad84
andriussvilys <andriussvilys@gmail.com>
2024-07-28 17:28:35 +0100

    add title and icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import Front from "../components/front/front";
+
+export const metadata: Metadata = {
+  title: 'Andrius Svilys | Software developer',
+  description: '...',
+}
 
 export default async function Page() {
   return (
